3725e193770b7c4438a6b74e8eee2228bb00feab
Abdulaziz Ayalew <162428984+AbdulazizAyalew@users.noreply.github.com>
2026-02-10 11:45:05 +0300

    feat: Add Comments for better Readability of Logic

--- LeetCode/EASY/Valid_Anagram.py
+++ LeetCode/EASY/Valid_Anagram.py
@@ -3,6 +3,7 @@ class Solution:
         count_s = Counter(s)
         count_t = Counter(t)
 
+        # Compare similarities of each key - value pair on both Dictionaries
         if count_t != count_s:
             return False
         return True
