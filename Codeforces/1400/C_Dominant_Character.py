48a5c7bd2e91f559f8e71ea3fd28fcfc62fd8b15
AbdulazizAya <abdulazizayalewmussa@gmail.com>
2026-02-10 14:56:19 +0300

    solve: CF - Dominant Character

---
+++ Codeforces/1400/C_Dominant_Character.py
@@ -0,0 +1,20 @@
+t = int(input())
+
+for i in range(t):
+    n = int(input())
+    s = input()
+
+    if "aa" in s:
+        print(2)
+        continue
+    elif "aba" in s or "aca" in s:
+        print(3)
+        continue
+    elif "abca" in s or "acba" in s:
+        print(4)
+        continue
+    elif "abbacca" in s or "accabba" in s:
+        print(7)
+        continue
+    else:
+        print(-1)
