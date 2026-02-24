7a53c5eaefd21361f84b59f18b22af7343291666
Abdulaziz Ayalew <162428984+AbdulazizAyalew@users.noreply.github.com>
2026-02-24 10:01:51 +0300

    solve : Leet - Divide Players into Teams of equal skill

--- LeetCode/Medium/2491.Divide_Players_into_Teams_of_equal_skill.py
+++ LeetCode/Medium/2491.Divide_Players_into_Teams_of_equal_skill.py
@@ -1,1 +1,18 @@
-solve : Leet - Divide Players into Teams of equal Skill
+class Solution:
+    def dividePlayers(self, skill: List[int]) -> int:
+        skill.sort()
+        l = 0
+        R = len(skill) - 1
+        summ = 0
+        Chem = 0
+        while l < R:
+            if l == 0:
+                summ = skill[l] + skill[R]
+            else:
+                if skill[l] + skill[R] != summ:
+                    Chem = -1
+                    break
+            Chem += (skill[l] * skill[R])
+            l += 1
+            R -= 1
+        return Chem 
