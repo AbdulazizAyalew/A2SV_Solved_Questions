65bce2ba4567d5714c3f22d46cc1e569a7aa66b6
Abdulaziz Ayalew <162428984+AbdulazizAyalew@users.noreply.github.com>
2026-04-20 01:34:08 +0300

    Add solution for Sort List

--- leetcode/0148-sort-list/0148-sort-list.py
+++ leetcode/0148-sort-list/0148-sort-list.py
@@ -1,4 +1,3 @@
-
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
@@ -6,49 +5,41 @@
 #         self.next = next
 class Solution:
     def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        def mergeSort(arr1,arr2):
-            l = 0
-            r = 0
-            new_arr = []
-            while l < len(arr1) and r < len(arr2):
-                if arr1[l] <= arr2[r]:
-                    new_arr.append(arr1[l])  
-                    l += 1
-                else:
-                    new_arr.append(arr2[r])
-                    r += 1    
-            new_arr.extend(arr1[l:])
-            new_arr.extend(arr2[r:])
-            return new_arr
+        def mergeSort(left,right):
+            Head = ListNode()
+            curr = Head
 
-        def merge(arr):
-            if len(arr) <= 1:
-                  return arr
+            while left and right:
+                if left.val <= right.val:
+                    curr.next = left
+                    left = left.next
+                else:
+                    curr.next = right
+                    right = right.next
+                
+                curr = curr.next
+            if left:
+                curr.next = left
+            if right:
+                curr.next = right
+            return Head.next
+        
+        def merge(node):
+            if not node or not node.next:
+                return node
             
-            n = len(arr) // 2
-            left = merge(arr[:n])
-            right = merge(arr[n:])
+            start = node
+            fast = node
+            while fast and fast.next and fast.next.next:
+                start = start.next
+                fast = fast.next.next
+            
+            mid = start.next
+            start.next = None
+            left = merge(node)
+            right = merge(mid)
 
             return mergeSort(left,right)
         
-        nums = []
-        ptr = head
-        while ptr:
-            nums.append(ptr.val)
-            ptr = ptr.next
-
-
-        arr = merge(nums)
-
-        result = []
-        First = ListNode()
-        for i in range(len(arr)):
-            curr = ListNode(arr[i])
-            if i == 0:
-                First = curr
-            else:
-                prev.next = curr
-            
-            prev = curr
-
-        return First if head else head
+        return merge(head)
+        
