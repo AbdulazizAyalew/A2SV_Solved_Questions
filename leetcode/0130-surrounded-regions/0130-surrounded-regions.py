af9103eb0eb631cc1e84907bf06c74a943875f6e
Abdulaziz Ayalew <162428984+AbdulazizAyalew@users.noreply.github.com>
2026-04-26 11:19:01 +0300

    Add solution for Surrounded Regions

--- leetcode/0130-surrounded-regions/0130-surrounded-regions.py
+++ leetcode/0130-surrounded-regions/0130-surrounded-regions.py
@@ -3,30 +3,26 @@ class Solution:
         """
         Do not return anything, modify board in-place instead.
         """
-        visited = set()
         directions = [(1,0),(-1,0),(0,1),(0,-1)]
-        def check_edge(i,j):
-            nonlocal surrounded
-            for x,y in directions:
-                ni,ny = i+x,j+y
-                if ni >= len(board) or ni < 0 or ny >= len(board[0]) or ny < 0:
-                    surrounded = False
+
         def dfs(i,j):
-            visited.add((i,j))
-            visited_reg.add((i,j))
-            check_edge(i,j)
+            board[i][j] = "T"
             for x,y in directions:
-                ni,ny = i+x, j+y
-                if 0 <= ni < len(board) and 0 <= ny < len(board[0]) and (ni,ny) not in visited and board[ni][ny] == "O":
+                ni,ny = i + x, j + y
+                if 0 <= ni < len(board) and 0 <= ny < len(board[0]) and board[ni][ny] != "T" and board[ni][ny] == "O":
                     dfs(ni,ny)
+            
         
         for i in range(len(board)):
             for j in range(len(board[0])):
-                if board[i][j] == "O" and (i,j) not in visited:
-                    surrounded = True
-                    visited_reg = set()
+                if (i == 0 or i == len(board) -1 or j == 0 or j == len(board[0]) - 1) and ( board[i][j] == "O" and board[i][j] != "T"):
                     dfs(i,j)
-                    if surrounded:
-                        for x,y in visited_reg:
-                            board[x][y] = "X"
-                                
+        
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                if board[i][j] == "T":
+                    board[i][j] = "O"
+                elif board[i][j] == "O":
+                    board[i][j] = "X"
+                else:
+                    continue
