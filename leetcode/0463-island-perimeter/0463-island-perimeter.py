b39dac54baf68e2293a3540dfcb554251ecbd436
Abdulaziz Ayalew <162428984+AbdulazizAyalew@users.noreply.github.com>
2026-04-26 09:56:29 +0300

    Add solution for Island Perimeter

--- leetcode/0463-island-perimeter/0463-island-perimeter.py
+++ leetcode/0463-island-perimeter/0463-island-perimeter.py
@@ -1,17 +1,35 @@
 class Solution:
-    def islandPerimeter(self, grid):
-        rows, cols = len(grid), len(grid[0])
+    def islandPerimeter(self, grid: List[List[int]]) -> int:
         perimeter = 0
+        visited = set()
+        directions = [(1,0),(-1,0),(0,1),(0,-1)]
 
-        for r in range(rows):
-            for c in range(cols):
-                if grid[r][c] == 1:
-                    perimeter += 4
+        def count_ones(i,j):
+            count = 0
+            for x,y in directions:
+                ni,ny = i+x,j+y
+                if 0 <= ni < len(grid) and 0 <= ny < len(grid[0]) and grid[i+x][j+y] == 1:
+                    count += 1
+            return count 
+        
+        def dfs(i,j):
+            nonlocal perimeter
+            visited.add((i,j))
+            if grid[i][j] == 1:
+                count = count_ones(i,j)
+                perimeter += (4-count)
 
-                    if c + 1 < cols and grid[r][c + 1] == 1:
-                        perimeter -= 2
+            for x,y in directions:
+                ni,ny = i+x,j+y
+                if 0 <= ni < len(grid) and 0 <= ny < len(grid[0])  and (i+x,j+y) not in visited and grid[ni][ny] == 1:
+                    dfs(ni,ny)
+        
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == 1:
+                    dfs(i,j)
+                    return perimeter
+        
+                
+                
 
-                    if r + 1 < rows and grid[r + 1][c] == 1:
-                        perimeter -= 2
-
-        return perimeter
