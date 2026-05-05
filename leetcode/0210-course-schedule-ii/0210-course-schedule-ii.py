0c77fee6e770feabeb1d75c7eafd7ba74f18c7bc
Abdulaziz Ayalew <162428984+AbdulazizAyalew@users.noreply.github.com>
2026-05-06 01:20:58 +0300

    Add solution for Course Schedule II

---
+++ leetcode/0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -0,0 +1,28 @@
+from collections import deque
+from typing import List
+
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        graph = [[] for _ in range(numCourses)]
+        indegree = [0] * numCourses
+        
+        for course, prereq in prerequisites:
+            graph[prereq].append(course)
+            indegree[course] += 1
+        
+        queue = deque([i for i in range(numCourses) if indegree[i] == 0])
+        order = []
+        
+        while queue:
+            current = queue.popleft()
+            order.append(current)
+            
+            for neighbor in graph[current]:
+                indegree[neighbor] -= 1
+                if indegree[neighbor] == 0:
+                    queue.append(neighbor)
+        
+        if len(order) == numCourses:
+            return order
+        
+        return []
